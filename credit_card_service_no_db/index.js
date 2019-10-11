c08da7dab45bf30b7fddfd331f2f2af6cc795520
amitmula <amitmula@gmail.com>
2019-10-11 22:04:10 +0530

    adding cors and removing poweredby express headers

--- credit_card_service_no_db/index.js
+++ credit_card_service_no_db/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const morgan = require('morgan')
-const config = require('config');
+const config = require('config')
 const app = express()
 
+app.disable('x-powered-by')
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+});
+
 app.use(require('./routes/index.routes'))
 
 // health check
@@ -16,4 +24,4 @@ app.get('/', (req, res) => {
 // Starting server
 app.listen(config.port)
 
-module.exports = app; // for testing
+module.exports = app // for testing
