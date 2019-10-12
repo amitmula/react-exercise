7456cac3b6b0f2de8a88b87532cbf43d3b093bda
amitmula <amitmula@gmail.com>
2019-10-12 08:04:32 +0530

    injecting the child components in parent

--- credit_card_processing_react_app/src/App.js
+++ credit_card_processing_react_app/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import AddNewCard from './components/NewCardForm'
+import CardList from './components/CardList'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <Header></Header>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} md={4} className="formArea">
+            <h3>Add</h3>
+            <AddNewCard></AddNewCard>
+          </Col>
+          <Col xs={12} md={8} className="listArea">
+            <h3>Existing Cards</h3>
+            <CardList></CardList>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
