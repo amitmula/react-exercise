417ac1ba05ef6b64028602b4d16655a56f1293e9
amitmula <amitmula@gmail.com>
2019-10-14 09:30:55 +0530

    adding enzyme tests to test component's state mutations

--- credit_card_processing_react_app/src/components/newCardForm/__test__/newCardForm.test.js
+++ credit_card_processing_react_app/src/components/newCardForm/__test__/newCardForm.test.js
@@ -3,29 +3,63 @@ import ReactDom from 'react-dom'
 
 import NewCardForm from '../newCardForm'
 
-import { render, cleanup } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import "@testing-library/jest-dom/extend-expect"
 import renderer from 'react-test-renderer'
 
-afterEach(cleanup)
+import { mount } from 'enzyme';
 
-it("renders without crashing", () => {
-    const div = document.createElement("div")
-    ReactDom.render(<NewCardForm />, div)
-    ReactDom.unmountComponentAtNode(div)
-})
+describe('newCardForm', () => {
 
-it("renders newCardForm with props", () => {
-    const { getByTestId } = render(<NewCardForm testProp="testPropVal" />)
-    expect(getByTestId('newCardForm').getAttribute("test-attribute")).toBe('testPropVal');
-})
+    it("renders without crashing", () => {
+        const div = document.createElement("div")
+        ReactDom.render(<NewCardForm />, div)
+        ReactDom.unmountComponentAtNode(div)
+    })
 
-it("renders cardList with props", () => {
-    const { getByTestId } = render(<NewCardForm testProp="some test val" />)
-    expect(getByTestId('newCardForm').getAttribute("test-attribute")).toBe('some test val');
-})
+    it("renders newCardForm with props", () => {
+        const { getByTestId } = render(<NewCardForm testProp="testPropVal" />)
+        expect(getByTestId('newCardForm').getAttribute("test-attribute")).toBe('testPropVal');
+    })
 
-it("matches snapshot", () => {
-    const tree = renderer.create(<NewCardForm testProp="testPropVal" />).toJSON()
-    expect(tree).toMatchSnapshot()
+    it("renders cardList with props", () => {
+        const { getByTestId } = render(<NewCardForm testProp="some test val" />)
+        expect(getByTestId('newCardForm').getAttribute("test-attribute")).toBe('some test val');
+    })
+
+    it("matches snapshot", () => {
+        const tree = renderer.create(<NewCardForm testProp="testPropVal" />).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+
+    it('shows success alert when card has been saved successfully', async (done) => {
+        const mockSuccessResponse = {
+            "message": "The card has been added",
+            "content": {
+                "id": 1000,
+                "name": "Test Card",
+                "number": "4375510276155014",
+                "limit": 4000,
+                "balance": 0
+            }
+        };
+        const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+        const mockFetchPromise = Promise.resolve({
+            ok: true,
+            json: () => mockJsonPromise,
+        });
+
+        jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise)
+        const wrapper = mount(<NewCardForm />);
+        wrapper.state().form = Object.assign({}, wrapper.state().form, { name: "some_name", number: "some_numer", limit: "some_limit" })
+        wrapper.find('form').simulate('submit');
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+        process.nextTick(() => {
+            expect(wrapper.state().message).toEqual("The card has been added")
+            expect(wrapper.state().alertBox).toEqual(true)
+            expect(wrapper.state().alertVarient).toEqual("success")
+            global.fetch.mockClear()
+            done()
+        });
+    })          //likewise such state mutation changes can bbbe tested for other components as well 
 })
